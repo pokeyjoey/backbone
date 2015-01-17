e2d2634857c0256f97636a9a7d41208ca8e170c1
Jeff Roche <jkeauctions@gmail.com>
2015-01-17 16:22:26 -0500

    Lynda.com bootstrap class through episode 3.1

--- app/views.py
+++ app/views.py
@@ -21,6 +21,34 @@ def index():
         user=user,
         posts=posts)
 
-@app.route('/test')
-def test():
-    return "route test" 
+@app.route('/grid')
+def grid():
+    return render_template('grid.html', title='Grid')
+
+@app.route('/index_02_02')
+def index_02_02():
+    return render_template('02_02.html', title='Grid')
+
+@app.route('/index_02_03')
+def index_02_03():
+    return render_template('02_03.html', title='Grid')
+
+@app.route('/index_02_04')
+def index_02_04():
+    return render_template('02_04.html', title='Grid')
+
+@app.route('/index_02_05')
+def index_02_05():
+    return render_template('02_05.html', title='Grid')
+
+@app.route('/index_02_06')
+def index_02_06():
+    return render_template('02_06.html', title='Grid')
+
+@app.route('/index_02_07')
+def index_02_07():
+    return render_template('02_07.html', title='Grid')
+
+@app.route('/index_03_01')
+def index_03_01():
+    return render_template('03_01.html', title='Grid')
