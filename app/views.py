a4cc35f6c230b60f1a9e348562c104c2f0e91ce1
Jeff Roche <jkeauctions@gmail.com>
2015-01-17 23:22:44 -0500

    Lynda.com bootstrap class though section 4

--- app/views.py
+++ app/views.py
@@ -52,3 +52,52 @@ def index_02_07():
 @app.route('/index_03_01')
 def index_03_01():
     return render_template('03_01.html', title='Grid')
+
+@app.route('/index_03_02')
+def index_03_02():
+    return render_template('03_02.html', title='Grid')
+
+@app.route('/index_03_03')
+def index_03_03():
+    return render_template('03_03.html', title='Grid')
+
+@app.route('/index_03_04')
+def index_03_04():
+    return render_template('03_04.html', title='Grid')
+
+@app.route('/index_03_05')
+def index_03_05():
+    return render_template('03_05.html', title='Grid')
+
+@app.route('/index_03_06')
+def index_03_06():
+    return render_template('03_06.html', title='Grid')
+
+@app.route('/index_03_07')
+def index_03_07():
+    return render_template('03_07.html', title='Grid')
+
+@app.route('/index_03_09')
+def index_03_09():
+    return render_template('03_09.html', title='Grid')
+
+@app.route('/index_03_08')
+def index_03_08():
+    return render_template('03_08.html', title='Grid')
+
+@app.route('/index_04_01')
+def index_04_01():
+    return render_template('04_01.html', title='Grid')
+
+@app.route('/index_04_02')
+def index_04_02():
+    return render_template('04_02.html', title='Grid')
+
+@app.route('/index_04_03')
+def index_04_03():
+    return render_template('04_03.html', title='Grid')
+
+@app.route('/index_04_04')
+def index_04_04():
+    return render_template('04_04.html', title='Grid')
+
